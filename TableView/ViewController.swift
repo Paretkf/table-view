c68ad5d1bcdac4ccd1e112ef8876f18d930a3218
princel3lcS <princeboy__@hotmail.com>
2019-02-12 11:46:01 +0700

    first commit

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -1,20 +1,27 @@
-//
-//  ViewController.swift
-//  TableView
-//
-//  Created by Admin on 12/2/2562 BE.
-//  Copyright © 2562 th.ac.kmutnb.www. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellIdentifier = "Cell"
+    let items = ["Albert", "beryl", "Charlie", "Derek"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel!.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected index \(indexPath.row) (\(items[indexPath.row]))")
     }
 
-
 }
-
